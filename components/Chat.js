656e83e28b3eb3b5e971962f1ada3fb9fa24ca55
rj-subhankar <subhankar.rj@gmail.com>
2021-04-22 23:33:29 +0530

    Implement chat server side rendering

--- components/Chat.js
+++ components/Chat.js
@@ -4,14 +4,19 @@ import { useCollection } from 'react-firebase-hooks/firestore';
 import styled from 'styled-components'
 import { auth, db } from '../firebase';
 import getRecipientEmail from '../utils/getRecipientEmail';
-
+import { useRouter } from 'next/router';
 function Chat({ id, users }) {
 
+    const router = useRouter();
     const [user] = useAuthState(auth);
     const [recipientSnapshot] = useCollection(db.collection('users').where('email', '==', getRecipientEmail(users, user)))
     const recipient = recipientSnapshot?.docs?.[0]?.data();
     const recipientEmail = getRecipientEmail(users, user);
-    return <Container>
+
+    const enterChat = () => {
+        router.push(`/chat/${id}`)
+    }
+    return <Container onClick={enterChat}>
         {recipient ? (
             <UserAvatar src={recipient?.photoURL} />
         ) : (
