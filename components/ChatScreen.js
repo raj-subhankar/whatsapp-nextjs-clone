93730ad68da2641df0a7f61a96113e4588c789a3
rj-subhankar <subhankar.rj@gmail.com>
2021-04-23 21:42:26 +0530

    Create ChatScreen

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,9 +1,68 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import styled from 'styled-components'
+import { auth, db } from '../firebase';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import MicIcon from '@material-ui/icons/Mic';
 
-function ChatScreen() {
+function ChatScreen({ chat, messages }) {
+    const [user] = useAuthState(auth);
+    const router = useRouter();
+    const [messagesSnapshot] = useCollection(
+        db
+            .collection('chats')
+            .doc(router.query.id)
+            .collection('messages')
+            .orderBy('timestamp', 'asc')
+        );
+
+    const showMessages = () => {
+        if(messagesSnapshot) {
+            return messagesSnapshot.docs.map(message => (
+                <Message 
+                    key={message.id}
+                    user={message.data().user}
+                    message={{
+                        ...message.data(),
+                        timestamp: message.data().timestamp?.toDate().getTime(),
+                    }}
+                />
+            ))
+        }
+    }
+
     return(
         <Container>
-            <h1>This is a chat</h1>
+            <Header>
+                <Avatar />
+                <HeaderInformation>
+                    <h3>Rec email</h3>
+                    <p>last seen ...</p>
+                </HeaderInformation>
+                <HeaderIcons>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </HeaderIcons>
+            </Header>
+
+            <MessageContainer>
+                {showMessages()}
+                <EndOfMessage />
+            </MessageContainer>
+
+            <InputContainer>
+                <InsertEmoticonIcon />
+                <Input />
+                <MicIcon />
+            </InputContainer>
         </Container>
     )
 }
@@ -12,4 +71,61 @@ export default ChatScreen
 
 const Container = styled.div`
 
+`;
+
+const Input = styled.input`
+    flex: 1;
+    outline: 0;
+    border: none;
+    border-radius: 10px;
+    background-color: whitesmoke;
+    padding: 20px;
+    margin-left: 15px;
+    margin-right: 15px;
+`;
+
+const InputContainer = styled.form`
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    position: sticky;
+    bottom: 0;
+    background-color: white;
+    z-index: 100;
+`;
+
+const Header = styled.div`
+    position: sticky;
+    background-color: white;
+    z-index: 100;
+    top: 0;
+    display: flex;
+    padding: 11px;
+    height: 80px;
+    align-items: center;
+    border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+    margin-left: 15px;
+    flex: 1;
+
+    > h3 {
+        margin-bottom: 3px;
+    }
+
+    > p {
+        font-size: 14px;
+        color: gray;
+    }
+`;
+
+const EndOfMessage = styled.div``;
+
+const HeaderIcons = styled.div``;
+
+const MessageContainer = styled.div`
+    padding: 30px;
+    background-color: #e5ded8;
+    min-height: 90vh;
 `;
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -91,7 +91,19 @@ const SidebarButton = styled(Button)`
 `;
 
 const Container = styled.div`
+    flex: 0.45;
+    border-right: 1px solid whitesmoke;
+    height: 100vh;
+    min-width: 300px;
+    max-width: 350px;
+    overflow-y: scroll;
 
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+    -ms-overflow-style: none;
+    scrollbar-width: none;
 `;
 
 const Header = styled.div`
